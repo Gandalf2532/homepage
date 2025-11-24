0ba6a38a19a3fb38c52df6727a2d25b34f0f453c
Gandalf2532 <fgier2010@gmail.com>
2025-11-24 14:53:48 +0100

    fixed typos

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="text-center bg-zinc-50 font-sans dark:bg-black bg-repeat" style={{minHeight:'100vh',backgroundImage: 'url(/background_img.png)',  backgroundColor: 'rgba(0, 0, 0, 0.6)', backgroundBlendMode: 'multiply' }}>
         <h1 className="text-4xl m-2">Informationen zur Impressumspflicht in Deutschland:</h1>
-        <p>Rein private Webseiten sind von der Impressumspflicht ausgenommen. Dazu uählt auch diese Seite. Kriterien für eine Private Liste:</p>
+        <p>Rein private Webseiten sind von der Impressumspflicht ausgenommen. Dazu zählt auch diese Seite. Kriterien für eine private Seite:</p>
         <ol>
             <li>ausschließlich persönliche Nutzung</li>
             <li>keinerlei kommerzielle Nutzung, Werbung sowie Affliliate-Links</li>
