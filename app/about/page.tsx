180527fba5ebc0eba6c58fd51c79d13057d9f366
Gandalf2532 <fgier2010@gmail.com>
2026-02-09 12:41:32 +0100

    Change text color to white in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="text-center bg-zinc-50 font-sans dark:bg-black bg-repeat" style={{minHeight:'100vh',backgroundImage: 'url(/background_img.png)',  backgroundColor: 'rgba(0, 0, 0, 0.6)', backgroundBlendMode: 'multiply' }}>
+    <div className="text-center bg-zinc-50 font-sans dark:bg-black bg-repeat text-white" style={{minHeight:'100vh',backgroundImage: 'url(/background_img.png)',  backgroundColor: 'rgba(0, 0, 0, 0.6)', backgroundBlendMode: 'multiply' }}>
         <h1 className="text-4xl m-2">Informationen zur Impressumspflicht in Deutschland:</h1>
         <p>Rein private Webseiten sind von der Impressumspflicht ausgenommen. Dazu zählt auch diese Seite. Kriterien für eine private Seite:</p>
         <ol>
